ad7bdbb39ad46d2d6b654ff16ad7a11941e62373
Adina Wagner <adina.wagner@t-online.de>
2020-09-21 17:08:04 +0200

    Add bash file to generate recipe via neurodocker package

---
+++ code/generate_simg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# Generate a singularity recipe with Neurodocker
+# requires the Python package neurodocker (pip install neurodocker)
+
+# Usage: bash code/generate_simg.sh <version-number>
+
+neurodocker generate singularity \
+--base=neurodebian:buster-non-free \
+--pkg-manager=apt  \
+--install datalad \
+--miniconda create_env=ml \
+            conda_install='python=3.8 scikitlearn scikit-image pandas numpy' \
+            activate=true \
+--entrypoint='/neurodocker/startup.sh' \
+> envs/Singularity.$1
